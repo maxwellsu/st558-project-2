6f0b4e2c3acdd64d54384816fa3a162ecc5d2196
Maxwell Su <mysu@ncsu.edu>
2023-10-09 04:41:56 -0400

    Changing df output type

--- output.R
+++ output.R
@@ -2,6 +2,6 @@ rmarkdown::render("cfbd_vignette.Rmd",
                   output_format = "github_document",
                   output_file = "README.md",
                   output_options = list(
-                    df_print = "paged"
+                    df_print = "tibble"
                   )
                 )
